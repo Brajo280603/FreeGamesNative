8dd897107a8c13b8668d2d69d3f291739862f5b5
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-11-10 17:43:11 +0530

    start

---
+++ android/app/src/main/java/com/lazydev/freeGames/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lazydev.freeGames;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import '../app.postcss';
+	import {AppBar, LightSwitch} from '@skeletonlabs/skeleton'
+	import { dev } from '$app/environment';
+	import { inject } from  '@vercel/analytics'
+
+	inject({ mode:dev? 'development': 'production'});
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">
+		<img class="h-14 invert dark:invert-0" src="/img/maskable_icon_bgremoved.png" alt="">
+	</svelte:fragment>
+	<h1 class="sm:text-3xl text-md uppercase ">Free Games</h1>
+	<svelte:fragment slot="trail">
+		<LightSwitch></LightSwitch>
+	</svelte:fragment>
+</AppBar>
+
+<slot />
